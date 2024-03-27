f6405a266509b7ddcc3c70e802afccb0cf072427
sleepyjirachi1 <tickethandler@outlook.com>
2024-03-27 22:49:00 +0000

    New Pallet Town Tech Guy Script

---
+++ assembly/overworld_scripts/pallet_town.s
@@ -0,0 +1,15 @@
+/*===== Pallet Town Overworld Scripts =====*/
+
+.thumb
+.align 2
+
+.include "../asm_defines.s"
+.include "../xse_commands.s"
+.include "../xse_defines.s"
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+.global EventScript_Technology_Guy
+EventScript_Technology_Guy:
+    msgbox gText_Technology_Guy MSG_FACE
+    end
